ce50e370cc9ebb6cbd835bbbc434c57f2571220a
Kachel Wittig <kjwittig@gmail.com>
2018-03-01 00:41:01 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,14 +2,8 @@ def turn_count(board)
   turn = 1
   board.each do |turn_number|
     puts "This is turn number #{turn_number}."
-    turn += 1
+    turn_number += 1
 end
-
-#
-# primary_colors.each do |color| # do begins a block
-#   # the lines between the do/end are the block's body
-#   puts "Primary Color #{color} is #{color.length} letters long."
-# end
 
 def current_player(board)
 end
