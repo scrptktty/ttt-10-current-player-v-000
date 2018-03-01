7be93cd02387e6b17713db3da6ebdc2b7ee9b60d
Kachel Wittig <kjwittig@gmail.com>
2018-03-01 01:31:32 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,14 +1,11 @@
+require "pry"
+
 def turn_count(board)
   turn_count = 0
-  board.each do |turn_number|
+  board.each do |space|
+    binding.pry
+    if space == "O" || space == "X"
     turn_count += 1
   end
+  turn_count
 end
-
-# brothers = ["Tim", "Tom", "Jim"]
-# counter = 1
-# brothers.each do |brother|
-#   puts "This is loop number #{counter}"
-#   puts "Stop hitting yourself #{brother}!"
-#   counter += 1
-# end
