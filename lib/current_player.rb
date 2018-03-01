01d5828edef82e36393e47384417c2bff5c08b02
Kachel Wittig <kjwittig@gmail.com>
2018-03-01 01:06:20 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,5 @@
 def turn_count(board)
-  turn_number = 0
-  board.each do |turn_number|
-    puts "This is turn number #{turn_number}."
-  turn_number += 1
-  end
+  board.each {|turn_number| puts "This is turn number #{turn_number}."}
 end
 
 # brothers = ["Tim", "Tom", "Jim"]
