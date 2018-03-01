91f8f39c2d81eaaa23a313d715c1b50050ea5be3
Kachel Wittig <kjwittig@gmail.com>
2018-03-01 00:55:00 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,8 @@
 def turn_count(board)
-  board.each {|turn_number| puts "This is turn number #{turn_number}."}
+  turn_number = 0
+  board.each do |turn_number| puts "This is turn number #{turn_number}."
+  turn_number += 1
+  end
 end
 
 # def current_player(board)
