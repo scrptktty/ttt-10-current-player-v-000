3dfae35e5adb936acc6328f173c5545db542af1c
Kachel Wittig <kjwittig@gmail.com>
2018-03-01 02:04:47 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -6,8 +6,12 @@ def turn_count(board)
   board.each do |space|
     turn_number = 0
     if space == "O" || "X"
-      return turn_number += 1
+      turn_number += 1
     end
       return turn_number
   end
+end
+
+def current_player
+  turn_number % 2 == 0 : "O" ? "X"
 end
