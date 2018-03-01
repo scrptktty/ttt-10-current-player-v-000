d995cccbe9ff817b8eff49b15d374f896b53ba7f
Kachel Wittig <kjwittig@gmail.com>
2018-03-01 01:34:00 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,10 +2,11 @@ require "pry"
 
 def turn_count(board)
   turn_count = 0
+
   board.each do |space|
-    binding.pry
     if space == "O" || space == "X"
     turn_count += 1
   end
+
   return turn_count
 end
