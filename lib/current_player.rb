6775fae92fc41599714595f8acaaa0363c378e59
Kachel Wittig <kjwittig@gmail.com>
2018-03-01 01:57:41 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,12 +3,10 @@ require = "pry"
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def turn_count(board)
-  turn = 0
   board.each do |space|
+    turn_number = 0
     if space == "O" || "X"
-      turn += 1
-      binding.pry
-      return turn
+      turn_number += 1
     end
   end
 end
