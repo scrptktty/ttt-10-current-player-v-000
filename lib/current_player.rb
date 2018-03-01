5493290e48b5f38f4326a22d8cc196d86fffa32f
Kachel Wittig <kjwittig@gmail.com>
2018-03-01 02:41:00 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,9 @@
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+board = ["O", " ", " ", " ", "X", " ", " ", " ", "X"]
 
 def turn_count(board)
   board.each do |space|
     turn_number = 0
-    if space == "X" || "O" 
+    if space == "X" || "O"
       turn_number += 1
     end
     return turn_number
